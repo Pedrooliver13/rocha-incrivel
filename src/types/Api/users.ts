962e6d7b75ab74a59d72545b79955b75089b9f97
pedrooliver13 <pedro@qyon.com>
2022-01-08 19:43:21 -0300

    fix: Change file extension

--- src/types/Api/users.ts
+++ src/types/Api/users.ts
@@ -1,4 +1,4 @@
-export type Props = {
+export type UserType = {
   id?: number;
   name?: string;
   BirthDate?: string;
--- src/types/templates/login.ts
+++
@@ -1,6 +0,0 @@
-export type Props = {
-  title: string;
-  subtitle?: string;
-  buttonLabel: string;
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-};
