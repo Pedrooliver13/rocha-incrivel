3ea092ee3d2cd8c0ada13a44c6c51b3b086a11f2
pedrooliver13 <pedro@qyon.com>
2022-01-08 20:18:21 -0300

    feat: Add roles in PrivateRoute
    if you don't have permission, you be redirected to home

--- src/routes/PrivateRoute/index.tsx
+++ src/routes/PrivateRoute/index.tsx
@@ -3,11 +3,22 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 import { PrivateRouteProps } from 'types/routes/privateRoutes';
 
-export const PrivateRoute: React.FC<PrivateRouteProps> = () => {
+export const PrivateRoute: React.FC<PrivateRouteProps> = ({ roles }) => {
   const token = Cookies.get('token');
-
   if (!token) {
     return <Navigate to="/sign-in" />;
+  }
+
+  if (roles && roles.length > 0) {
+    const userRoles = JSON.parse(token).roles;
+
+    if (!userRoles || !userRoles.length) {
+      return <Navigate to="/" />;
+    }
+
+    if (!userRoles.some((role: string) => roles.includes(role))) {
+      return <Navigate to="/" />;
+    }
   }
 
   return <Outlet />;
