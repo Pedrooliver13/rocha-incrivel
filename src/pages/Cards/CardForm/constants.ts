b3ea524e347ba50fea70c4d5ae8b874b3208aec3
pedrooliver13 <pedro@qyon.com>
2022-01-12 17:24:51 -0300

    feat: Create page CardForm

---
+++ src/pages/Cards/CardForm/constants.ts
@@ -0,0 +1,45 @@
+import * as Yup from 'yup';
+
+import { requiredFieldMessage } from 'helpers/commonMessages';
+
+export const EUserEnabledFeatures = {
+  card: 0
+};
+
+export const validationSchema = (id?: string | number) =>
+  Yup.object().shape({
+    name: Yup.string().required(requiredFieldMessage),
+    digits: Yup.string()
+      .length(4, 'Favor inserir 4 Dígitos')
+      .required(requiredFieldMessage),
+    limit: Yup.string()
+      .length(4, 'Favor inserir 4 Dígitos')
+      .required(requiredFieldMessage),
+    userId: id ? Yup.string().required(requiredFieldMessage) : Yup.string()
+  });
+
+export const initialBeforeState = {
+  status: 'requested',
+  metadatas: {
+    name: '',
+    digits: 0,
+    limit: 0
+  },
+  createAt: new Date(),
+  updateAt: null
+};
+
+export const radioOptions = [
+  {
+    optionLabel: 'Solicitado',
+    optionValue: 'requested'
+  },
+  {
+    optionLabel: 'Aprovado',
+    optionValue: 'approved'
+  },
+  {
+    optionLabel: 'Rejeitado',
+    optionValue: 'rejected'
+  }
+];
---
+++ src/types/pages/cardForm.ts
@@ -0,0 +1,7 @@
+export type CardFormProps = {
+  userId: number | string;
+  name: string;
+  digits: number | string;
+  limit: number | string;
+  status: string;
+};
