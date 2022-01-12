aa21225e8aa24b8ecf3adeedd4a49c04cf136938
pedrooliver13 <pedro@qyon.com>
2022-01-12 17:22:41 -0300

    feat: Add methods for use api, and type files
    create audits, cards and users

--- src/types/Api/audits.ts
+++ src/types/Api/audits.ts
@@ -1,19 +1,21 @@
 type logsType = {
   id?: number;
   name?: string;
-  digits?: number;
+  digits?: number | string;
   status?: string;
   metadatas?: {
     name?: string;
-    digits?: number;
+    digits?: number | string;
   };
-  createdAt?: string;
+  createdAt?: Date | string;
 };
 
 export type AuditType = {
-  id: number;
+  id?: number;
+  name?: string;
   type?: string;
   before?: logsType;
   after?: logsType;
+  createdAt?: Date | string;
   requestedBy?: number;
 };
